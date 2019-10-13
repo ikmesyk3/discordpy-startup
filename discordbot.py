5662ee2c88b18bd54085f0efd7a1715cd8114efc
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 01:13:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,8 @@ async def on_command_error(ctx, error):
     await ctx.send(str(error))
     
 @bot.command()
-async def start(client):
-  await client.change_presence (activity=game)
-  game = discord.game(str('Hello,World!'))
+async def change_status(self):
+    await self.bot.change_presence(game=discord.Game(name="Test")
 
 
 @bot.command()
