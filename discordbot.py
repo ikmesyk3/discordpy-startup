7a96eb0be316e753b27a780b8d88b186b7c8543c
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 05:59:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.client()
 
   
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
     
@@ -19,12 +19,12 @@ async def on_join_member(member):
   await member.send('hello,world')
     
     
-@bot.command()
+@client.command()
 async def (client):
   await client.change_presence (activity=game)
   game=discord.game(name(str('Hello,World!')))
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
     
