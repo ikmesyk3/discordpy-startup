4ebed8f2e8163af07114c0edb597b7a802f021c5
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 00:40:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,9 +15,5 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-@bot.start()
-async def start(ctx):
-  await client.change_presence(activity=game)
     
 bot.run(token)
