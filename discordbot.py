a001fa1100c5e14989212f937ea7c2c1bcae82d3
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 01:17:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,8 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+class discord.ActivityType(playing)
+
   
 @bot.event
 async def on_command_error(ctx, error):
