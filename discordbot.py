0e8ef5526416e99dad766fb114a89c5059991206
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 00:26:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,13 +6,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.status
-async def on_ready():
-  await change_presence(status=discord.Status.idle,activity=game)
-  game = discord.game(str('Hello,World!'))
-  
-  class discord.game(name(str('Hello,World!')
   
 @bot.event
 async def on_command_error(ctx, error):
@@ -22,6 +15,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    await change_presence(status=discord.Status.idle,activity=game)
+    game = discord.game(str('Hello,World!'))
+  
+    class discord.game(name(str('Hello,World!')
+
 
 
 bot.run(token)
