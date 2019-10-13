e14c7801e348e8f35821d00bcc8a95801a5ec3c3
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 00:23:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.status
 async def on_ready():
   await change_presence(status=discord.Status.idle,activity=game)
-  game = discord.game("Hello,World!")
+  game = discord.game(str("Hello,World!"))
   
 @bot.event
 async def on_command_error(ctx, error):
