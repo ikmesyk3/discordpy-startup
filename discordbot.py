2c765d2cac88209707be1dee351b18a019844cb9
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 04:25:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,17 @@ from discord.ext import commands
 import os
 import traceback
 
+import discord
 
+class MyClient(discord.Client):
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+            
+            
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
