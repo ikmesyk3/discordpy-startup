f79a5b1be8d4f2bf2aadc484df1b904a4cbf6b55
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-13 17:32:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,13 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+await client.change_presence(activity=discord.Game(name='my game'))
+
+# or, for watching:
+activity = discord.Activity(name='my activity', type=discord.ActivityType.watching)
+await client.change_presence(activity=activity)
+
+
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
