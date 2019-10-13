0b74712486eeb9569dd7aefe13c771a5f7b7b0c4
ikmesyk3 <56501780+ikmesyk3@users.noreply.github.com>
2019-10-14 05:37:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 import os
 import traceback
 
+class discord.client(activity):
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
   
